089ef1a0c8132fe60be9e8ab8f9ff2bd2f6a21cc
vkoottay <vkoottay@oracle.com>
2015-06-25 12:13:30 +0530

    Cleanup1

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,8 +3,6 @@ package helloworld;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    // This branch is for APS without T2P
     System.out.println("Hello World!!");
-    System.out.println("GIT Work");
   }
 }
