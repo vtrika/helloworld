a4cc42a77a8f1f4ae3c65f64da485aa0f65fd983
vtrika <vipincec@gmail.com>
2015-06-25 11:54:16 +0530

    Update HelloWorld.java

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,12 +4,7 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     // This branch is for APS without T2P
-    // New change1
-    // New change2
     System.out.println("Hello World!!");
-    // New change3
-    // aps change
-    // conflict change 
-    // conflict change
+    System.out.println("GIT Work");
   }
 }
