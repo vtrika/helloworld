bd5045e47a7e27a2996293a4bad2541a5062c383
vkoottay <vkoottay@oracle.com>
2015-06-25 10:45:39 +0530

    Add change for APS

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    // 
+    // This branch is for APS without T2P
     System.out.println("Hello World!!");
   }
 }
