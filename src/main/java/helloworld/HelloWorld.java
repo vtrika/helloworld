3dc858f4a0c038c52253b24f03da92b0d72865e5
vtrika <vipincec@gmail.com>
2015-06-28 20:10:31 +0530

    Update HelloWorld.java

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,11 +3,6 @@ package helloworld;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    // comment 1
     System.out.println("Hello World!!");
-    
-    // vipin
   }
-  
-  // commment 2
 }
