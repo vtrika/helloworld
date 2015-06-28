f1016591a41d543cdb0409bd9543f69f6d0f4bd2
vkoottay <vkoottay@oracle.com>
2015-06-28 19:58:31 +0530

    Change 1

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,8 +4,5 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello World!!");
-    System.out.println("GIT Hello World!!");
-    // change 1
-    // change 2
   }
 }
