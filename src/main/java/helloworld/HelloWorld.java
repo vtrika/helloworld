a0b5b48a35eedb97b48b4390d3eefde6225a8ec2
vtrika <vipincec@gmail.com>
2015-06-28 20:32:24 +0530

    Update HelloWorld.java

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,15 +3,6 @@ package helloworld;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    // print
     System.out.println("Hello World!!");
-  }
-  
-  public void method1() {
-    // placeholder method
-  }
-  
-  public void method1(String s1) throws Exception {
-    // TODO implement
   }
 }
